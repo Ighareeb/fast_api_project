fd8ef3cd7c7af623ab2ecd5b5cb3d1d9e38eafc7
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2024-04-15 20:35:08 +0400

    using query params

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Any
 from fastapi import FastAPI
 from enum import Enum
 
@@ -24,9 +25,9 @@ async def get_users():
     return {"message": "List all users"}
 
 
-@app.get("/users/{id}")
-async def get_user(id: int):
-    return {"message": f"Get user with id {id}"}
+@app.get("/users/{user_id}")
+async def get_user(user_id: int):
+    return {"message": f"Get user with id {user_id}"}
 
 
 class FoodEnum(str, Enum):
@@ -43,3 +44,42 @@ async def get_food(food_name: FoodEnum):
         return {"food name": food_name, "message": "Healthy Fruits"}
     if food_name == FoodEnum.dairy:
         return {"food name": food_name, "message": "Milk Products"}
+
+
+items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items")
+async def get_items(skip: int = 0, limit: int = 10):
+    return items_db[skip : skip + limit]
+
+
+# FastAPI recognizes path params and query params (since they are primitive types)
+# has optional query parameters --> 'q' which can be None + short which is bool (that adds description to item dict when T)
+# old way to declare optional query params --> Optional[str] = None
+@app.get("/items/{item_id}")
+async def get_item(
+    item_id: str, query_param: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "query_param": query_param}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update({"description": "This a description for the queried item"})
+    return item  # FastAPI will convert dict and return response as JSON object
+
+
+@app.get("/users/{user_id}/items/{items_id}")
+async def get_user_item(
+    user_id: int, items_id: int, q: str | None = None, short: bool = False
+):
+    item: dict[str, Any] = {"item_id": items_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {
+                "description": "This is a description for the queried item that belongs to the user"
+            }
+        )
+    return item
